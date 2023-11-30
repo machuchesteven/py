3a00017e21746de4b1186b91a305bec40b7b83ee
Machuche Steven <machuchesteven@gmail.com>
2023-11-30 09:53:22 +0300

    Updated DB to using postgresql

--- cinemaxy/cinemaxy/settings.py
+++ cinemaxy/cinemaxy/settings.py
@@ -22,7 +22,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a$+vo^6!^5afmychz9ud^^-qfgnrgs6!+bws^1u)ov=0fs16ac'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'cinemaxy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
